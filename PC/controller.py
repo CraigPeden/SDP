cb957399782a31de9e3780151f09e2b1a0c58dba
kostadinkalinov <kostadim@abv.bg>
2015-02-02 12:05:31 +0000

    Needs to be remapped to our robot

--- PC/controller.py
+++ PC/controller.py
@@ -254,37 +254,6 @@ class Attacker_Controller(Robot_Controller):
         comm.write('A_RUN_ENGINE %d %d\n' % (0, 0))
 
 
-class Arduino:
-
-    def __init__(self, port, rate, timeOut, comms):
-        self.serial = None
-        self.comms = comms
-        self.port = port
-        self.rate = rate
-        self.timeout = timeOut
-        self.setComms(comms)
-
-    def setComms(self, comms):
-        if comms > 0:
-            self.comms = 1
-            if self.serial is None:
-                try:
-                    self.serial = serial.Serial(self.port, self.rate, timeout=self.timeout)
-                except:
-                    print "No Arduino detected!"
-                    print "Continuing without comms."
-                    self.comms = 0
-                    #raise
-        else:
-            #self.write('A_RUN_KICK\n')
-            self.write('A_RUN_ENGINE %d %d\n' % (0, 0))
-            #self.write('D_RUN_KICK\n')
-            self.write('D_RUN_ENGINE %d %d\n' % (0, 0))
-            self.comms = 0
-
-    def write(self, string):
-        if self.comms == 1:
-            self.serial.write(string)
 
 
 if __name__ == '__main__':
