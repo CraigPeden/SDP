f062140e98a5c7f26d6654a3686b35b690f5e24f
kostadinkalinov <kostadim@abv.bg>
2015-02-03 01:25:46 +0000

    Fixing the file so that we can use it with our robot. Needs Calibration

--- PC/planning/utilities.py
+++ PC/planning/utilities.py
@@ -9,6 +9,29 @@ MAX_ANGLE_SPEED = 50
 BALL_VELOCITY = 3
 
 
+#constants that we will use below - please note that they should be calibrated
+
+#we will use this speed when we want to do rotation
+#the goal is to have a predictable behavior
+#and control the rotation by the duration period
+#of the run of the motors
+MOTOR_ROTATION_SPEED = 3
+
+#we will use this speed when we want to move forwards and backwards
+#the goal is to control the length of the movement by the duration
+#period of the run of the motors
+MOTOR_MOVE_SPEED = 5
+
+#this is the time that the motors need to run at MOTOR_ROTATION_SPEED in order to turn
+#the robot at an angle of pi/5
+ROTATION_DURATION_CONSTANT = 0.5
+
+#this is the time that the motors need to run at MOTOR_MOVE_SPEED in order to move
+#the robot at a distance of 50
+MOTOR_MOVE_DURATION_CONSTANT = 1
+
+
+
 def is_shot_blocked(world, our_robot, their_robot):
     '''
     Checks if our robot could shoot past their robot
@@ -86,20 +109,25 @@ def predict_y_intersection(world, predict_for_x, robot, full_width=False, bounce
 
 
 def grab_ball():
-    return {'left_motor': 0, 'right_motor': 0, 'kicker': 0, 'catcher': 1, 'speed': 1000}
+    return {'left_motor_speed': 0, 'right_motor_speed': 0, 'kicker_activated': 0, 'catcher_activated': 1, 'duration': 0.5}
 
 
 def kick_ball():
-    return {'left_motor': 0, 'right_motor': 0, 'kicker': 1, 'catcher': 0, 'speed': 1000}
+    return {'left_motor_speed': 0, 'right_motor_speed': 0, 'kicker_activated': 1, 'catcher_activated': 0, 'duration': 0.5}
 
 
 def open_catcher():
-    return {'left_motor': 0, 'right_motor': 0, 'kicker': 1, 'catcher': 0, 'speed': 1000}
+       return {'left_motor_speed': 0, 'right_motor_speed': 0, 'kicker_activated': 1, 'catcher_activated': 0, 'duration': 0.5}
 
 
 def turn_shoot(orientation):
-    return {'turn_90': orientation, 'left_motor': 0, 'right_motor': 0, 'kicker': 1, 'catcher': 0, 'speed': 1000}
-
+    turn_duration = 2.5 * ROTATION_DURATION_CONSTANT #since pi/2 is 2.5 * pi/5
+    #if orientation == -1 then turn left 90 degrees, i.e. pi/2 and shoot
+    if orientation == -1 :
+     return {'left_motor_speed': -MOTOR_ROTATION_SPEED, 'right_motor_speed': MOTOR_ROTATION_SPEED, 'kicker_activated': 1, 'catcher_activated': 0, 'duration': turn_duration}
+   #else turn right 90 degrees and shoot
+    else :
+     return {'left_motor_speed': MOTOR_ROTATION_SPEED, 'right_motor_speed': -MOTOR_ROTATION_SPEED, 'kicker_activated': 1, 'catcher_activated': 0, 'duration': turn_duration}
 
 def has_matched(robot, x=None, y=None, angle=None,
                 angle_threshold=ANGLE_MATCH_THRESHOLD, distance_threshold=DISTANCE_MATCH_THRESHOLD):
@@ -112,43 +140,48 @@ def has_matched(robot, x=None, y=None, angle=None,
     return dist_matched and angle_matched
 
 
-def calculate_motor_speed(displacement, angle, backwards_ok=False, careful=False):
+def calculate_motor_speed(distance, angle, backwards_ok=False, careful=False):
     '''
     Simplistic view of calculating the speed: no modes or trying to be careful
     '''
     moving_backwards = False
-    general_speed = 95 if careful else 300
     angle_thresh = BALL_ANGLE_THRESHOLD if careful else ANGLE_MATCH_THRESHOLD
 
     if backwards_ok and abs(angle) > pi/2:
         angle = (-pi + angle) if angle > 0 else (pi + angle)
         moving_backwards = True
 
-    if not (displacement is None):
+    if not (distance is None):
 
-        if displacement < DISTANCE_MATCH_THRESHOLD:
-            return {'left_motor': 0, 'right_motor': 0, 'kicker': 0, 'catcher': 0, 'speed': general_speed}
+        if distance < DISTANCE_MATCH_THRESHOLD:
+             return {'left_motor_speed': 0, 'right_motor_speed': 0, 'kicker_activated': 0, 'catcher_activated': 0, 'duration': 1}
 
         elif abs(angle) > angle_thresh:
-            speed = (angle/pi) * MAX_ANGLE_SPEED
-            return {'left_motor': -speed, 'right_motor': speed, 'kicker': 0, 'catcher': 0, 'speed': general_speed}
+            duration = (abs(angle)/(pi/5)) * ROTATION_DURATION_CONSTANT
+            if angle > 0 :
+              return {'left_motor_speed': -MOTOR_ROTATION_SPEED, 'right_motor_speed': MOTOR_ROTATION_SPEED, 'kicker_activated': 0, 'catcher_activated': 0, 'duration': duration}
+            elif angle <0 :
+              return {'left_motor_speed': MOTOR_ROTATION_SPEED, 'right_motor_speed': -MOTOR_ROTATION_SPEED, 'kicker_activated': 0, 'catcher_activated': 0, 'duration': duration}
 
         else:
-            speed = log(displacement, 10) * MAX_DISPLACEMENT_SPEED
-            speed = -speed if moving_backwards else speed
+            motor_run_duration = log(distance, 10) * MOTOR_MOVE_DURATION_CONSTANT
+            speed = -MOTOR_MOVE_SPEED if moving_backwards else MOTOR_MOVE_SPEED
             # print 'DISP:', displacement
             if careful:
-                return {'left_motor': speed, 'right_motor': speed, 'kicker': 0, 'catcher': 0, 'speed': 1000/(1+10**(-0.1*(displacement-85)))}
-            return {'left_motor': speed, 'right_motor': speed, 'kicker': 0, 'catcher': 0, 'speed': 1000/(1+10**(-0.1*(displacement-30)))}
+                return {'left_motor': speed, 'right_motor': speed, 'kicker': 0, 'catcher': 0, 'duration': motor_run_duration}
+            return {'left_motor': speed, 'right_motor': speed, 'kicker': 0, 'catcher': 0, 'duration': motor_run_duration}
 
     else:
 
         if abs(angle) > angle_thresh:
-            speed = (angle/pi) * MAX_ANGLE_SPEED
-            return {'left_motor': -speed, 'right_motor': speed, 'kicker': 0, 'catcher': 0, 'speed': general_speed}
+            duration = (abs(angle)/(pi/5)) * ROTATION_DURATION_CONSTANT
+            if angle > 0 :
+              return {'left_motor_speed': -MOTOR_ROTATION_SPEED, 'right_motor_speed': MOTOR_ROTATION_SPEED, 'kicker_activated': 0, 'catcher_activated': 0, 'duration': duration}
+            elif angle <0 :
+              return {'left_motor_speed': MOTOR_ROTATION_SPEED, 'right_motor_speed': -MOTOR_ROTATION_SPEED, 'kicker_activated': 0, 'catcher_activated': 0, 'duration': duration}
 
         else:
-            return {'left_motor': 0, 'right_motor': 0, 'kicker': 0, 'catcher': 0, 'speed': general_speed}
+             return {'left_motor_speed': 0, 'right_motor_speed': 0, 'kicker_activated': 0, 'catcher_activated': 0, 'duration': 1}
 
 
 
